96904b2732a791b8153a1d74c977cf0cb1552fba
anubhav bisht <anubhavbisht98@gmail.com>
2025-10-14 06:09:51 +0530

    completed login method

--- apps/jobber/src/app/auth/auth.service.ts
+++ apps/jobber/src/app/auth/auth.service.ts
@@ -4,12 +4,32 @@ import { Response } from 'express';
 import { UsersService } from '../users/users.service';
 import { compare } from 'bcryptjs';
 import { User } from '../users/models/user.model';
+import { ConfigService } from '@nestjs/config';
+import { TokenPayload } from './interface/token.payload';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService
+  ) {}
   async login({ email, password }: LoginInput, res: Response): Promise<any> {
     const user = await this.validateUser(email, password);
+    const expires = new Date();
+    expires.setMilliseconds(
+      expires.getTime() +
+        parseInt(this.configService.getOrThrow('JWT_EXPIRATION_TIME'), 10)
+    );
+    const tokenPayload: TokenPayload = { userId: user.id };
+    const accessToken = this.jwtService.sign(tokenPayload);
+    res.cookie('Authentication', accessToken, {
+      httpOnly: true,
+      expires,
+      secure: this.configService.get('NODE_ENV') === 'production',
+    });
+    return user;
   }
 
   private async validateUser(email: string, password: string): Promise<User> {

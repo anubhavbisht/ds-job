6efb7fa482553e5cea3d7eb5a049c2ed994e0b65
anubhav bisht <anubhavbisht98@gmail.com>
2025-10-15 18:54:33 +0530

    setting up job service

--- apps/jobber-jobs/src/app/jobs/jobs.module.ts
+++ apps/jobber-jobs/src/app/jobs/jobs.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FibonacciJob } from './fibonacci.job';
+import { DiscoveryModule } from '@golevelup/nestjs-discovery';
+import { JobsService } from './jobs.service';
 
-@Module({})
-export class JobsModule {
-  providers: [FibonacciJob];
-}
+@Module({
+  imports: [DiscoveryModule],
+  providers: [FibonacciJob, JobsService],
+})
+export class JobsModule {}

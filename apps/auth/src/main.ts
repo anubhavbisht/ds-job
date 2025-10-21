58c3a0a109a70782e1e00ece88f9aee30a37f5c3
anubhav bisht <anubhavbisht98@gmail.com>
2025-10-21 14:52:57 +0530

    restructuring codebase

---
+++ apps/auth/src/main.ts
@@ -0,0 +1,26 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { GrpcOptions, Transport } from '@nestjs/microservices';
+import { AUTH_PACKAGE_NAME } from 'types/proto/auth';
+import { join } from 'path';
+import { nestInit } from '@jobber/nestjs';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await nestInit(app, 'auth');
+  app.connectMicroservice<GrpcOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: AUTH_PACKAGE_NAME,
+      protoPath: join(__dirname, 'proto/auth.proto'),
+    },
+  });
+  await app.startAllMicroservices();
+}
+
+bootstrap();
---
+++ apps/jobs/src/main.ts
@@ -0,0 +1,15 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { nestInit } from '@jobber/nestjs';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await nestInit(app, 'jobs');
+}
+
+bootstrap();

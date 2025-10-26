c98bfb84c2ba980bf5454652f795634821ef38a1
anubhav bisht <anubhavbisht98@gmail.com>
2025-10-26 22:39:54 +0530

    batching days

--- apps/executor/src/app/jobs/clickhouse/response-analytics/etl.constants.ts
+++ apps/executor/src/app/jobs/clickhouse/response-analytics/etl.constants.ts
@@ -1,6 +1,6 @@
 export const ETL_METADATA_TABLE = 'etl_metadata';
 
-export const BATCH_MS = 1000 * 60 * 60 * 24 * 5;
+export const BATCH_MS = 1000 * 60 * 60 * 24 * 10;
 export const OVERLAP_MS = 45 * 60 * 1000;
 export const CONCURRENCY = 1;
 
